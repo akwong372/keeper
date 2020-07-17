b90afc67492dd0b5b1342f38dacbd2bae4dbec97
akwong372 <akwong372@gmail.com>
2020-07-17 10:58:03 -0700

    add area to create notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
-import notes from '../notes';
-
-const renderNotes = (note, i) => {
-    return (
-        <Note key={'note'+i} title={note.title} content={note.content} />
-    );
-};
+import CreateArea from "./CreateArea";
 
 const App = () => {
+
+    const [notes, setNotes] = useState([]);
+
+    const addNote = note => {
+        return setNotes(prev => [...prev, note]);
+    }
+
+    const renderNotes = (note, i) => {
+        return (
+            <Note key={'note' + i} title={note.title} content={note.content} />
+        );
+    };
 
     return (
         <div>
             <Header />
+            <CreateArea addNote={addNote} />
             {notes.map(renderNotes)}
             <Footer />
         </div>
