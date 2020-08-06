c7ab2576cfaa1cfc72ffb3611e22937e7d05e3fb
akwong372 <akwong372@gmail.com>
2020-08-06 16:43:24 -0700

    logs out user when button clicked

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,10 @@ import BookmarkIcon from '@material-ui/icons/Bookmark';
 const Header = () => {
     return (
         <header>
-            <h1><BookmarkIcon/>Keeper</h1>
-            <button>log out</button>
+            <h1><BookmarkIcon />Keeper</h1>
+            <button>
+                <a href="http://localhost:8080/logout">log out</a>
+            </button>
         </header>
     );
 };
